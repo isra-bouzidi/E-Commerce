62dcd145a37c0604298047d90042a2bd2d419669
isra-bouzidi <b.isra2004@gmail.com>
2025-11-18 22:14:17 +0100

    feature-3/EC: adding category from Admin Dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 route::get('/',[HomeController::class,'home']);
 
@@ -21,3 +22,8 @@ require __DIR__.'/auth.php';
 
 
 route::get('admin/dashboard',[HomeController::class,'index'])->middleware(['auth','admin']);
+
+
+route::get('view_category',[AdminController::class,'view_category'])->middleware(['auth','admin']);
+
+route::post('add_category',[AdminController::class,'add_category'])->middleware(['auth','admin']);
