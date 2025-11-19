8efb3bff83f21e9e5ffd8aad6e401312f3cdd84d
Isra Bouzidi <b.isra2004@gmail.com>
2025-11-19 22:46:18 +0100

    feature-7/EC: Sweet Alert Delete Confirmation (#7)

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -96,7 +96,7 @@
               <tr>
                 <td>{{$data->category_name}}</td>
                 <td>
-                  <a class="btn btn-danger" href="{{url('delete_category',$data->id)}}">Delete</a>
+                  <a class="btn btn-danger" onClick="confirmation(event)" href="{{url('delete_category',$data->id)}}">Delete</a>
                 </td>
               </tr>
               
@@ -104,10 +104,55 @@
 
             </table>
 
+
+            </div>
+
+
+
+
             </div>
       </div>
     </div>
     <!-- JavaScript files-->
+     <script type="text/javascript">
+
+      function confirmation(ev)
+      {
+        ev.preventDefault();
+
+        var urlToRedirect = ev.currentTarget.getAttribute('href');
+
+        console.log(urlToRedirect);
+
+
+        swal({
+          
+          title: "Are you sure to delete this?",
+          text: "This delete will be parmanent.",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+
+        })
+
+        .then((willCancel)=>{
+
+          if(willCancel)
+          {
+            window.location.href = urlToRedirect;
+          }
+
+
+        });
+
+      }
+     </script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
     <script src="/admincss/vendor/jquery/jquery.min.js"></script>
     <script src="/admincss/vendor/popper.js/umd/popper.min.js"> </script>
     <script src="/admincss/vendor/bootstrap/js/bootstrap.min.js"></script>
