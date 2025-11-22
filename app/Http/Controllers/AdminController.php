8a1e9a20295d7b147593c323d01893af270661c7
isra-bouzidi <b.isra2004@gmail.com>
2025-11-22 22:31:01 +0100

    feature-9/EC: Upload Product Data to Database Table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\Category;
 
+use App\Models\Product;
+
 class AdminController extends Controller
 {
     public function view_category()
@@ -65,5 +67,58 @@ class AdminController extends Controller
         toastr()->timeOut(10000)->closeButton()->success('Category Updated Successfully.');
 
         return redirect('view_category');
+    }
+
+    public function add_product()
+    {
+
+
+        $category = Category::all();
+
+        return view('admin.add_product', compact('category'));
+    }
+
+
+
+    public function upload_product(Request $request)
+    {
+        
+        $data = new Product;
+
+        $data->title = $request->title;
+
+        $data->description = $request->description;
+
+        $data->price = $request->price;
+
+        $data->quantity = $request->qty;
+
+        $data->category = $request->category;
+
+        
+        $image = $request->image;
+        
+        if($image)
+
+        {
+            $imagename = time().'.'.$image->getClientOriginalExtension();
+
+            $request->image->move('products', $imagename);
+
+            $data->image = $imagename;
+
+        }
+
+
+
+
+        $data->save();
+
+        toastr()->timeOut(10000)->closeButton()->success('Product Added Successfully.');
+
+        return redirect()->back();
+
+
+
     }
 }
