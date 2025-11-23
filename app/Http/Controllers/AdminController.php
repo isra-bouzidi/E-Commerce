bee3ebabc30fdee16b590a85fb1b9a3cabbee238
isra-bouzidi <b.isra2004@gmail.com>
2025-11-23 17:38:11 +0100

    feature-12/EC: Short Description

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -125,7 +125,7 @@ class AdminController extends Controller
 
     public function view_product()
     {
-        $product = Product::paginate(5);
+        $product = Product::paginate(3);
         return view('admin.view_product', compact('product'));
     }
 }
