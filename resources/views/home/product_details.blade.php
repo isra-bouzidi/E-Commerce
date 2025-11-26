80fe099251bf872fda51e68ed07adb420fc43793
Isra Bouzidi <b.isra2004@gmail.com>
2025-11-26 18:43:08 +0100

    feature-18/EC: Display Product Details (#18)

---
+++ resources/views/home/product_details.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  @include('home.css')
+
+  <style type="text/css">
+
+    .div_center
+    {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 30px;
+    }
+
+    .detail-box
+    {
+        padding: 15px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+
+    @include('home.header')
+
+    <!-- end header section -->
+
+  </div>
+
+
+
+<!-- Product Details Start -->
+
+
+<section class="shop_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Latest Products
+        </h2>
+      </div>
+      <div class="row">
+
+     
+
+        <div class="col-md-12">
+          <div class="box">
+          
+              <div class="div_center">
+                <img width="400" src="/products/{{$data->image}}" alt="">
+              </div>
+
+
+              <div class="detail-box">
+                <h6>{{$data->title}}</h6>
+                <h6>Price
+                  <span>${{$data->price}}</span>
+                </h6>
+              </div>
+
+
+
+              <div class="detail-box">
+                <h6>Category:{{$data->category}}</h6>
+                <h6>Availabnle Quantity
+                  <span>{{$data->quantity}}</span>
+                </h6>
+              </div>
+
+
+              <div class="detail-box">
+                  <p>{{$data->description}}</p>
+                
+              </div>
+            
+
+              
+            
+          </div>
+        </div>
+
+        
+
+
+        
+        
+      </div>
+      
+    </div>
+  </section>
+
+
+
+    
+<!-- Product Details end -->
+   
+
+
+
+  <!-- info section -->
+  @include('home.footer')
+
+  <!-- end info section -->
+
+
+  <script src="{{('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{('js/bootstrap.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="{{('js/custom.js')}}"></script>
+
+</body>
+
+</html>
