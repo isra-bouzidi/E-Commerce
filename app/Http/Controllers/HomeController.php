2e28c721b6d9ae5666c816bbf75c9fff4f7d83b9
Isra Bouzidi <b.isra2004@gmail.com>
2025-12-18 13:32:16 +0100

    feature-19/EC: Add Product to Cart (#19)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 use App\Models\Product;
 
+use App\Models\User;
+
+use App\Models\Cart;
+
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function index()
@@ -36,5 +42,31 @@ class HomeController extends Controller
         $data = Product::find($id);
 
         return view('home.product_details', compact('data'));
+    }
+
+    public function add_cart($id)
+    {
+    
+        $product_id = $id;
+
+        $user = Auth::user();
+
+        $user_id = $user->id;
+
+        $data = new Cart;
+
+        $data->user_id = $user_id;
+
+        $data->product_id = $product_id;
+
+        $data->save();
+
+
+        toastr()->timeOut(10000)->closeButton()->success('Product added to cart successfully!');
+
+
+        return redirect()->back();
+
+
     }
 }
